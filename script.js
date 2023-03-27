c374b39287fe1c87b8e795ec68ca958c562e6d31
Neal <victor.negocios.l.17@gmail.com>
2023-03-27 13:26:46 -0500

    creacion del elemento lista utilizando createElement y el appenchild

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const btn = document.querySelector("[data-form-btn]");
+
+const createTask= (evento)=>{
+    evento.preventDefault();//sirve para que no se refresque la pagina cuandose inicie el evento
+    const name_tarea = document.querySelector("[data-form-input]");
+    const value = name_tarea.value;
+    name_tarea.value ="";
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+
+    const content = `
+    <div>
+      <i class="far fa-check-square icon"></i>
+      <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML= content;
+
+  list.appendChild(task);
+    console.log(value);
+}
 console.log(btn);
 
-btn.addEventListener("click", function() {
-    let name_tarea = document.querySelector("[data-form-input]").value;
-    console.log(name_tarea);
-});
+btn.addEventListener("click", createTask);
